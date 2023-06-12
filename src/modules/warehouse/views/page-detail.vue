e08b194a29f0514a9d97f4ebd403a7034aeea611
Martien Dermawan Tanama <martiendt@gmail.com>
2023-06-12 15:44:52 +0700

    fix notification font

--- src/modules/warehouse/views/page-detail.vue
+++ src/modules/warehouse/views/page-detail.vue
@@ -33,7 +33,7 @@ const onDelete = async () => {
   if (confirm('Are you sure want to delete this data?')) {
     const result = await axios.delete(`/v1/warehouses/${route.params.id}`)
     if (result.status === 204) {
-      notification('Delete warehouse data success', '', { type: TypesEnum.Success })
+      notification('', 'Delete warehouse data success', { type: TypesEnum.Success })
       router.push('/warehouse')
     }
   }
