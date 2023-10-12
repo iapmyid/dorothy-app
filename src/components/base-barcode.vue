f7999166cead388d37043548d882ca8634c238ee
martiendt <martiendt@gmail.com>
2023-10-12 20:39:41 +0700

    adjust font size

--- src/components/base-barcode.vue
+++ src/components/base-barcode.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useNumeric } from '@/composable/numeric'
 import JsBarcode from 'jsbarcode'
-import { onMounted, ref, watch } from 'vue'
+import { onMounted, ref, watch, computed } from 'vue'
 
 const numeric = useNumeric()
 
@@ -44,18 +44,31 @@ watch(
     })
   }
 )
+
+const fontSize = computed(() => {
+  if (props.label.length < 19) {
+    return '12px'
+  }
+  if (props.label.length < 21) {
+    return '11px'
+  }
+  if (props.label.length < 23) {
+    return '10px'
+  }
+  return '9px'
+})
 </script>
 
 <template>
   <div
-    class="flex flex-col text-center justify-center items-center relative text-black uppercase font-bold bg-white w-40 h-81px px-2"
+    class="flex flex-col text-center justify-center items-center relative text-black uppercase bg-white w-40 h-81px px-2 overflow-hidden"
   >
-    <span class="text-10px z-1 leading-tight line-clamp-2" v-if="props.showName">{{ props.label }}</span>
+    <span :style="{ fontSize }" class="z-1 leading-tight line-clamp-2" v-if="props.showName">{{ props.label }}</span>
     <svg ref="barcodeRef" id="barcode" class="-my-2"></svg>
-    <div class="text-9px leading-none flex justify-between space-x-3 z-1 max-w-full" v-if="props.showCode">
+    <div class="text-8px leading-none flex justify-between space-x-3 z-1 max-w-full" v-if="props.showCode">
       <div>{{ props.value }}</div>
       <div class="flex-1 whitespace-nowrap overflow-hidden">{{ props.size }} - {{ props.color }}</div>
     </div>
-    <div class="text-10px leading-tight w-full text-center">Rp. {{ numeric.format(props.sellingPrice) }}</div>
+    <div class="text-14px leading-tight w-full text-center">Rp. {{ numeric.format(props.sellingPrice) }}</div>
   </div>
 </template>
